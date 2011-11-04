4c1c747c41590f68379bd7d8915a2261a0f2bfa3
mattias.nilsson <mattias.nilsson@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-04 07:58:31 +0000

    Added static info for language translation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mn_episerver2typo3/trunk@53683 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables_static+adt.sql
+++ ext_tables_static+adt.sql
@@ -1,1 +1,23 @@
-INSERT INTO tx_mnepiserver2typo3_episerver_language_translation (episerver_language_code, typo3_language_code) VALUES('sv', 'se');
+#
+# Table structure for table 'tx_mnepiserver2typo3_episerver_language_translation'
+#
+DROP TABLE IF EXISTS tx_mnepiserver2typo3_episerver_language_translation;
+CREATE TABLE tx_mnepiserver2typo3_episerver_language_translation (
+    uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumtext,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+    episerver_language_code tinytext,
+    typo3_language_code tinytext,
+    
+    PRIMARY KEY (uid),
+    KEY parent (pid)
+);
+
+INSERT INTO tx_mnepiserver2typo3_episerver_language_translation (pid, tstamp, crdate, cruser_id, deleted, hidden, episerver_language_code, typo3_language_code) VALUES (0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 0, 0, 'sv', 'se');
